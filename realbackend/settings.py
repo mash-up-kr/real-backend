9065a87668a54e9d62f7b598eb619058a9196ffa
s2zan <209rrsz@gmail.com>
2019-12-17 17:45:56 +0900

    [ENV] DB PostgreSql로 변경(#5)
    - setting에 DATABASES 수정

--- realbackend/settings.py
+++ realbackend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'realbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'real',
+        'USER': 'real',
+        'PASSWORD': 'dreamticket',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
