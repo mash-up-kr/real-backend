b9d6c5eb86264bcc226136d1f8a73427dc4c870b
s2zan <209rrsz@gmail.com>
2019-12-29 03:43:43 +0900

    [FEAT] url 설정 (#11)
    - dreams/, dreams/history/, dreams/{pk}/, dreams/{pk}/result/

---
+++ real/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import DreamListCreateAPIView, DreamRetrieveUpdateDestroyAPIView, ResultRetrieveUpdateDestroyAPIView, \
+    DreamHistoryListAPIView
+
+urlpatterns = format_suffix_patterns([
+    path('', DreamListCreateAPIView.as_view(), name='dream_list'),
+    path('history/', DreamHistoryListAPIView.as_view(), name='dream_list'),
+    path('<int:pk>/', DreamRetrieveUpdateDestroyAPIView.as_view(), name='dream_detail'),
+    path('<int:pk>/result/', ResultRetrieveUpdateDestroyAPIView.as_view(), name='dream_detail'),
+])
